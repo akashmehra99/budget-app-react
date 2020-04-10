be6ff7c97dec5dbaa75eed32bd7fa7777a07af53
Akash MEHRA <akash.mehra99@gmail.com>
2020-04-10 23:55:58 +0530

    splitted single redux file in to seprate actions, reducers, selectors and store and importted the same in app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import store from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import AppRoutes from './routers/AppRoutes';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 
+const appStore = store;
+appStore.subscribe(() => console.log(appStore.getState()));
+
+appStore.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: -21000, note: 'For Dec month' }));
+appStore.dispatch(addExpense({ description: 'Gas bill', amount: 2000, createdAt: -11000, note: 'For Dec month' }));
+appStore.dispatch(setTextFilter('water bill'));
+
+const state = appStore.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log('Visible Expenses ->', visibleExpenses);
 
 ReactDOM.render(<AppRoutes />, document.getElementById('app'));
---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers } from 'redux';
+import expensesReducer  from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
+
+// Store creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+export default store;
